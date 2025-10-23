6dce54a17641e34c2bb231f72699a9773c65c0e0
ilic-k <stako2908@gmail.com>
2025-10-23 15:06:26 +0200

    Edit server routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,9 @@ import { connectDB } from './config/db';
 import path from 'path';
 import authRoutes from './routers/auth.router';
 import userRouter from './routers/user.router';
+import adminRouter from './routers/admin.router';
+import cottageRouter from './routers/cottage.router';
+import reservationRouter from './routers/reservation,router';
 
 const app = express()
 
@@ -13,8 +16,18 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRouter);
+app.use('/api/cottages', cottageRouter);
+app.use('/api/reservations', reservationRouter);
+app.use('/api/admin', adminRouter);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err);
+  res.status(500).json({ message: 'Internal server error', error: err?.message });
+});
 
 app.get('/', (req,res)=> {res.send("Server is running")})
 app.listen(4000, ()=>console.log('Express running on port 4000'))
