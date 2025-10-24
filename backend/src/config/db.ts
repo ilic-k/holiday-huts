2bfd03ad260a347f792ecb630887b7f32ca7488c
ilic-k <stako2908@gmail.com>
2025-10-24 16:20:36 +0200

    Change database name to pia2425

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/pia');
+    await mongoose.connect('mongodb://localhost:27017/pia2425');
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err);
